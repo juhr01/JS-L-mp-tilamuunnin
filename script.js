781eec4b5bb0c270a5da5edce630495832cc155d
juhr01 <juho1.riipinen@gmail.com>
2022-05-09 12:46:17 +0300

    yritystä

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
 const mylist = document.querySelector("#myList");
-const valittu = mylist.options[mylist.selectedIndex].text;
+const valittu = mylist.options[mylist.selectedIndex].value;
 const myForm = document.querySelector('#my-form');
+const lampotila = document.querySelector('#lampotila').value;
 
-myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e) {
-    e.preventDefault();
+
+function muunna() {
+
+    if (valittu == "cTOf") {
+        cTOf();
+    } else if (valittu == "cTOk") {
+        cTOk();
+    } else if (valittu == "fTOc") {
+        fTOc();
+    } else if (valittu == "fTOk") {
+        fTOk();
+    } else if (valittu == "kTOc") {
+        kTOc();
+    } else {
+        kTOf();
+    }
+  
 }
 
-function cTOf(celsius) {
-    const cTemp = celsius;
+mylist.addEventListener("change", (e) => {
+    const value = e.target.value;
+   
+    if (value) {
+      muunna();
+    }
+  });
+
+function cTOf() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const cTemp = parseFloat(lampotila);
     const cToFahr = (cTemp * 1.8) + 32;
     document.querySelector("#output").innerHTML = cToFahr;
 }
 
-function cTOk(celsius) {
-    const cTemp = celsius;
+function cTOk() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const cTemp = parseFloat(lampotila);
     const cToKel = cTemp + 273.15;
     document.querySelector("#output").innerHTML = cToKel;
 }
 
-function fTOc(fahrenheit) {
-    const fTemp = fahrenheit;
+function fTOc() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const fTemp = parseFloat(lampotila);
     const fToCel = (fTemp - 32) / 1.8;
     document.querySelector("#output").innerHTML = fToCel;
 }
 
-function fTOk(fahrenheit) {
-    const fTemp = fahrenheit;
+function fTOk() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const fTemp = parseFloat(lampotila);
     const fToKel = ((fTemp - 32) / 1.8) + 273.15;
     document.querySelector("#output").innerHTML = fToKel;
 }
 
-function kTOc(kelvin) {
-    const kTemp = kelvin;
+function kTOc() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const kTemp = parseFloat(lampotila);
     const kToCel = kTemp -273.15;
     document.querySelector("#output").innerHTML = kToCel;
 }
 
-function kTOf(kelvin) {
-    const kTemp = kelvin;
+function kTOf() {
+    const lampotila = document.querySelector('#lampotila').value;
+    const kTemp = parseFloat(lampotila);
     const kToFahr = ((kTemp -273.15) * 1.8) + 32;
     document.querySelector("#output").innerHTML = kToFahr;
 }
