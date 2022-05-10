fe7939341cf5daad7df4c18d31599be8710a64cd
juhr01 <juho1.riipinen@gmail.com>
2022-05-10 12:24:20 +0300

    muunnin toimii

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const mylist = document.querySelector("#myList");
 const myForm = document.querySelector('#my-form');
 const msg = document.querySelector('.msg');
-const pyorista = (number, decimalPlaces) => {
-    const factorOfTen = Math.pow(10, decimalPlaces);
-    return Math.round(number * factorOfTen) / factorOfTen;
-  }
 
 function muunna() {
 
@@ -24,31 +20,33 @@ function muunna() {
 
 }
 
+function pyorista(number, decimalPlaces) {
+    const factorOfTen = Math.pow(10, decimalPlaces);
+    const a = Math.round(number * factorOfTen) / factorOfTen;
+    return a;
+  }
+
 function cTOf() {
     const lampotila = document.querySelector('#lampotila').value;
     const cTemp = parseFloat(lampotila);
     const cToFahr = (cTemp * 1.8) + 32;
-    if (cTemp < -273.15) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
-    /*} else if (document.querySelector('#1d').checked == false || document.querySelector('#2d').checked == false || document.querySelector('#3d').checked == false ) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Valitse muunnoksen tarkkuus';
-        setTimeout(() => msg.remove(), 3000); */
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (cTemp < -273.15) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        if (document.querySelector('#1d').checked == true) {
-            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 1)} &#176;F`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 1).toFixed(1)} &#176;F`;
         }
-        if (document.querySelector('#2d').checked == true) {
-            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 2)} &#176;F`;
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 2).toFixed(2)} &#176;F`;
         }
-        if (document.querySelector('#3d').checked == true) {
-            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 3)} &#176;F`;
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToFahr, 3).toFixed(3)} &#176;F`;
         }
     }
 }
@@ -57,16 +55,24 @@ function cTOk() {
     const lampotila = document.querySelector('#lampotila').value;
     const cTemp = parseFloat(lampotila);
     const cToKel = cTemp + 273.15;
-    if (cTemp < -273.15) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (cTemp < -273.15) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${cToKel} &#176;K`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToKel, 1).toFixed(1)} &#176;K`;
+        }
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToKel, 2).toFixed(2)} &#176;K`;
+        }
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${cTemp} &#176;C = ${pyorista(cToKel, 3).toFixed(3)} &#176;K`;
+        }
     }
 }
 
@@ -74,16 +80,24 @@ function fTOc() {
     const lampotila = document.querySelector('#lampotila').value;
     const fTemp = parseFloat(lampotila);
     const fToCel = (fTemp - 32) / 1.8;
-    if (fTemp < -459.67) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (fTemp < -459.67) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');;
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${fToCel} &#176;C`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToCel, 1).toFixed(1)} &#176;C`;
+        }
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToCel, 2).toFixed(2)} &#176;C`;
+        }
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToCel, 3).toFixed(3)} &#176;C`;
+        }
     }
 }
 
@@ -91,16 +105,24 @@ function fTOk() {
     const lampotila = document.querySelector('#lampotila').value;
     const fTemp = parseFloat(lampotila);
     const fToKel = ((fTemp - 32) / 1.8) + 273.15;
-    if (fTemp < -459.67) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (fTemp < -459.67) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${fToKel} &#176;K`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToKel, 1).toFixed(1)} &#176;K`;
+        }
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToKel, 2).toFixed(2)} &#176;K`;
+        }
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${fTemp} &#176;F = ${pyorista(fToKel, 3).toFixed(3)} &#176;K`;
+        }
     }
 }
 
@@ -108,16 +130,24 @@ function kTOc() {
     const lampotila = document.querySelector('#lampotila').value;
     const kTemp = parseFloat(lampotila);
     const kToCel = kTemp - 273.15;
-    if (kTemp < 0) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (kTemp < 0) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${kToCel} &#176;C`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToCel, 1).toFixed(1)} &#176;C`;
+        }
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToCel, 2).toFixed(2)} &#176;C`;
+        }
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToCel, 3).toFixed(3)} &#176;C`;
+        }
     }
 }
 
@@ -125,15 +155,23 @@ function kTOf() {
     const lampotila = document.querySelector('#lampotila').value;
     const kTemp = parseFloat(lampotila);
     const kToFahr = ((kTemp - 273.15) * 1.8) + 32;
-    if (kTemp < 0) {
-        msg.classList.add('error');
-        msg.innerHTML = 'Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste';
-        setTimeout(() => msg.remove(), 3000);
-    } else if (typeof lampotila !== "number") {
-        msg.classList.add('error');
-        msg.innerHTML = 'Syötä pelkästään numeroita';
-        setTimeout(() => msg.remove(), 3000);
+    if (lampotila == '') {
+        alert('Syötä lämpötila!');
+    } else if (kTemp < 0) {
+        alert('Lämpötila ei voi olla pienempi kuin absoluuttinen nollapiste!');
+    } else if (isNaN(lampotila)) {
+        alert('Syötä ainoastaan numeroita!');
+    } else if (document.getElementById('1d').checked == false && document.getElementById('2d').checked == false && document.getElementById('3d').checked == false) {
+        alert('Valitse muunnoksen tarkkuus!');
     } else {
-        document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${kToFahr} &#176;F`;
+        if (document.getElementById('1d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToFahr, 1).toFixed(1)} &#176;F`;
+        }
+        if (document.getElementById('2d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToFahr, 2).toFixed(2)} &#176;F`;
+        }
+        if (document.getElementById('3d').checked == true) {
+            document.querySelector("#output").innerHTML = `${kTemp} &#176;K = ${pyorista(kToFahr, 3).toFixed(3)} &#176;F`;
+        }
     }
 }
